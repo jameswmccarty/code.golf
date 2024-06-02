dabace238889ac32eeaa83ca1519f4f3ef3c744a
jameswmccarty <jameswmccarty@gmail.com>
2024-06-02 19:53:12 -0400

    Update Pernicious_Numbers.py

--- Pernicious_Numbers.py
+++ Pernicious_Numbers.py
@@ -1,5 +1,4 @@
-p=lambda x:x>1 and not any(x%i==0 for i in range(2,x))
-n=lambda x:p(bin(x).count('1'))
-for i in range(51):
+r,p,n=range,lambda x:x>1and not any(x%i==0 for i in r(2,x)),lambda x:p(bin(x).count('1'))
+for i in r(51):
 	if n(i):
 		print(i)
