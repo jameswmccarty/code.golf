c3bcddd65cf64214d51095876fb8da7caee7668d
jameswmccarty <jameswmccarty@gmail.com>
2024-06-02 19:46:57 -0400

    Update Prime_Numbers.py

--- Prime_Numbers.py
+++ Prime_Numbers.py
@@ -1,4 +1,5 @@
-p=lambda x: not any(x%i==0 for i in range(2,x))
-for a in range(2,101):
+r,z=range,print
+p=lambda x: not any(x%i==0 for i in r(2,x))
+for a in r(2,10001):
 	if p(a):
-		print(a)
+		z(a)
