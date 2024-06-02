b68307e3ac64ba9feaeb557004c21bb840df848e
jameswmccarty <jameswmccarty@gmail.com>
2024-06-02 19:59:28 -0400

    Update Ascending_Primes.py

--- Ascending_Primes.py
+++ Ascending_Primes.py
@@ -1,6 +1,7 @@
-b=lambda x: len(set(str(x)))==len(str(x))and list(str(x))==sorted(list(str(x)))
-p=lambda x: not any(x%i==0 for i in range(2,x))
-print('2')
-for a in range(3,23456790,2):
+l,s,t,r,q,o=len,set,list,range,print,str
+b=lambda x:l(s(o(x)))==l(o(x))and t(o(x))==sorted(t(str(x)))
+p=lambda x:not any(x%i==0 for i in r(2,x))
+q('2')
+for a in r(3,23456790,2):
 	if b(a)and p(a):
-		print(a)
+		q(a)
