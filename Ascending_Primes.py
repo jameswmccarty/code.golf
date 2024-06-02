80b973edf1673f4b177a9b023c540d58db9155fe
jameswmccarty <jameswmccarty@gmail.com>
2024-06-02 16:59:59 -0400

    Create Ascending_Primes.py

---
+++ Ascending_Primes.py
@@ -0,0 +1,6 @@
+b=lambda x: len(set(str(x)))==len(str(x)) and list(str(x))==sorted(list(str(x)))
+p=lambda x: not any(x%i==0 for i in range(2,x))
+print('2')
+for a in range(3,23456790,2):
+	if b(a) and p(a):
+		print(a)
