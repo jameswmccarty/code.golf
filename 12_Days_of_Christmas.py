6d97edcac564c2b9c5814f1028a97a4755c9a921
jameswmccarty <jameswmccarty@gmail.com>
2024-06-02 19:45:54 -0400

    Update 12_Days_of_Christmas.py

--- 12_Days_of_Christmas.py
+++ 12_Days_of_Christmas.py
@@ -1,10 +1,9 @@
-g=("A Partridge in a Pear Tree.","Two Turtle Doves, and","Three French Hens,","Four Calling Birds,","Five Gold Rings,","Six Geese-a-Laying,","Seven Swans-a-Swimming,","Eight Maids-a-Milking,","Nine Ladies Dancing,","Ten Lords-a-Leaping,","Eleven Pipers Piping,","Twelve Drummers Drumming,")
-s='On the'
+g,z=("A Partridge in a Pear Tree.","Two Turtle Doves, and","Three French Hens,","Four Calling Birds,","Five Gold Rings,","Six Geese-a-Laying,","Seven Swans-a-Swimming,","Eight Maids-a-Milking,","Nine Ladies Dancing,","Ten Lords-a-Leaping,","Eleven Pipers Piping,","Twelve Drummers Drumming,"),print
 for i,d in enumerate(('First','Second','Third','Fourth','Fifth','Sixth','Seventh','Eighth','Ninth','Tenth','Eleventh','Twelfth')):
-	print(s,d,"day of Christmas\nMy true love sent to me")
-	if i==0:
-		s='\n'+s
+	if i>0:
+		z()
+	z("On the",d,"day of Christmas\nMy true love sent to me")
 	n=i
 	while n>-1:
-		print(g[n])
+		z(g[n])
 		n-=1
