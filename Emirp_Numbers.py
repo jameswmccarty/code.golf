73195ed568e99a34b95e0a67cefa52387ce04d15
jameswmccarty <jameswmccarty@gmail.com>
2024-06-03 21:00:18 -0400

    Update Emirp_Numbers.py

--- Emirp_Numbers.py
+++ Emirp_Numbers.py
@@ -1,5 +1,6 @@
 r,s=range,str
 p=lambda x:not any(x%i==0 for i in r(2,x))
-for a in r(2,1001):
-	if (s(a)[::-1]!=s(a))&p(a)&p(int(s(a)[::-1])):
+for a in r(2,10001):
+	b=s(a)[::-1]
+	if (b!=s(a))&p(a)&p(int(b)):
 		print(a)
