8cacfafff3a80c50ba2bd35aef42229f8215c57e
jameswmccarty <jameswmccarty@gmail.com>
2024-06-30 20:12:45 -0400

    Update Zeckendorf_Representation.py

--- Zeckendorf_Representation.py
+++ Zeckendorf_Representation.py
@@ -1,13 +1,10 @@
 import sys
-def b(n):
-	f=[1,1]
-	while f[-1]<=n:f+=[sum(f[-2:])]
-	return f[::-1][1:]
+f=[1,1]
+while f[0]<2**31:f=[f[0]+f[1]]+f
 d=n=0
 def s(a,b):
-	global d,n
+	global d
 	if d:return
 	if sum(b)==n:print(' + '.join(str(x)for x in b));d=1
 	if a and sum(b)<n:s(a[2:],b+[a[0]]);s(a[1:],b)
-for g in sys.argv[1:]:
-	d,n=0,int(g);s(b(n),[])
+for g in sys.argv[1:]:d,n=0,int(g);s(f,[])
