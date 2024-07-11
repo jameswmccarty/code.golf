2449c04cf836e9677b4bead902932f52d588d8c5
jameswmccarty <jameswmccarty@gmail.com>
2024-07-10 21:35:10 -0400

    Update Happy_Numbers.py

--- Happy_Numbers.py
+++ Happy_Numbers.py
@@ -1,5 +1,4 @@
-h=lambda x:sum(pow(int(y),2)for y in str(x))
 for i in range(200):
-	a,n=[],h(i)
-	while n not in a:a+=[n];n=h(n)
+	a,n=[],i
+	while n not in a:a+=[n];n=sum(int(y)**2for y in str(n))
 	if 1 in a:print(i)
