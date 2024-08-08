d72d6ef82a47b4e696a3ca374c5fed3b8134277d
jameswmccarty <jameswmccarty@gmail.com>
2024-08-08 09:24:22 -0400

    Update Fizz_Buzz.py

--- Fizz_Buzz.py
+++ Fizz_Buzz.py
@@ -1,3 +1,5 @@
 for i in range(1,101):
-	t,f,z,b=i%3==0,i%5==0,'Fizz','Buzz'
-	print(z+b if t&f else(z if t else(b if f else i)))
+	o=''
+	if i%3==0:o+='Fizz'
+	if i%5==0:o+='Buzz'
+	print(o if o else i)
