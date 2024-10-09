68f577df7a0c60ba9793a5d637062175b9f9fdc0
jameswmccarty <jameswmccarty@gmail.com>
2024-10-08 20:37:00 -0400

    Update Foo_Fizz_Buzz_Bar.py

--- Foo_Fizz_Buzz_Bar.py
+++ Foo_Fizz_Buzz_Bar.py
@@ -1,7 +1,2 @@
-for i in range(1,1001):
-	o=''
-	if i%2==0:o+='Foo'
-	if i%3==0:o+='Fizz'
-	if i%5==0:o+='Buzz'
-	if i%7==0:o+='Bar'
-	print(o if o else i)
+i=1
+while i<1001:o=''.join(v for k,v in((2,'Foo'),(3,'Fizz'),(5,'Buzz'),(7,'Bar'))if i%k==0);print(o if o else i);i+=1
