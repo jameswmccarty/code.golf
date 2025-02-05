4fa76327c5ab03a12c8d37a4f9a383d139550405
jameswmccarty <jameswmccarty@gmail.com>
2025-02-05 14:12:17 -0500

    Update Tic-tac-toe.py

--- Tic-tac-toe.py
+++ Tic-tac-toe.py
@@ -1,3 +1,3 @@
 import sys
-def t(a):return 3*a in b or any(all(b[i]==a for i in x)for x in[[0,5,10],[2,5,8],[0,4,8],[1,5,9],[2,6,10]])
-for b in sys.argv[1:]:print([['-','X'][t('X')],'O'][t('O')])
+d,t='O',lambda a:3*a in b or any(all(b[i]==a for i in x)for x in[[0,5,10],[2,5,8],[0,4,8],[1,5,9],[2,6,10]])
+for b in sys.argv[1:]:print(['-X'[t('X')],d][t(d)])
