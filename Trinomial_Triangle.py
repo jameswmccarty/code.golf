963c1b66ad72a51b5d40f6d6bc3730dafbf8dc67
jameswmccarty <jameswmccarty@gmail.com>
2025-09-18 20:38:28 -0400

    Update Trinomial_Triangle.py

--- Trinomial_Triangle.py
+++ Trinomial_Triangle.py
@@ -1,3 +1,3 @@
-a,c,z=[1]*3,[0,0],print
-z(1)
-for i in range(19):z(*a);a=c+a+c;a=[sum(a[j:j+3])for j in range(len(a)-2)]
+a=[1]*3
+print(1)
+while len(a)<40:print(*a);a=[0,0]+a;a=[sum(a[j:j+3])for j in range(len(a))]
