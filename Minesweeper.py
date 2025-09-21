21ccd242de76e47113d3e3af6c5be213e5e33c5e
jameswmccarty <jameswmccarty@gmail.com>
2025-09-21 13:36:10 -0400

    Update Minesweeper.py

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,8 +1,8 @@
 import sys
-r=[0,1,2,3,4,5,6,7,8,9]
+r,k=[0,1,2,3,4,5,6,7,8,9],[-1,0,1]
 def c(x,y,b,t=0):
- for j in(-1,0,1):
-  for i in (-1,0,1):
+ for j in k:
+  for i in k:
    if i|j:
     if(x+j in r)&(y+i in r):t+=b[y+i][x+j]=='M'
  return[t,'X'][b[y][x]=='M']
