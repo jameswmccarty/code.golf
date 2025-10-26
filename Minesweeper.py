d77e22f899669727eff8f9cd7b59430ace96bde0
jameswmccarty <jameswmccarty@gmail.com>
2025-10-26 15:12:30 -0400

    Update Minesweeper.py

--- Minesweeper.py
+++ Minesweeper.py
@@ -2,5 +2,5 @@ import sys
 r,k=[0,1,2,3,4,5,6,7,8,9],[-1,0,1]
 for b in sys.argv[1:]:
  b=b.split()
- for y in r:print(''.join(str([sum(b[y+i][x+j]=='M'if(x+j in r)&(y+i in r)&(i|j)else 0for i in k for j in k),'X'][b[y][x]=='M'])for x in r))
+ for y in r:print(''.join(str([sum(b[y+i][x+j]=='X'if(x+j in r)&(y+i in r)&(i|j)else 0for i in k for j in k),'F'][b[y][x]=='X'])for x in r))
  print()
