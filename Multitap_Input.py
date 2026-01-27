0b6da26ffd9d9166555720957d5dd9d764280e19
jameswmccarty <jameswmccarty@gmail.com>
2026-01-27 09:31:59 -0500

    Update Multitap_Input.py

--- Multitap_Input.py
+++ Multitap_Input.py
@@ -1,9 +1,9 @@
 import sys
 def e(x,c,o,l):
  for u in x+l:
-  if u==l:c+=1
-  else:
+  if u!=l:
    if l!=' ':o+=chr(ord("  ADGJMPTW"[int(l)])+c)
-   c=0;l=u
+   c=-1;l=u
+  c+=1
  return o
 for a in sys.argv[1:]:print(e(a,0,'',' '))
