6731ff43246e635327acd17de8aa025a985494f9
jameswmccarty <jameswmccarty@gmail.com>
2026-09-23 20:22:54 -0400

    Update Chess_Move_Validation.py

--- Chess_Move_Validation.py
+++ Chess_Move_Validation.py
@@ -2,4 +2,4 @@ import sys
 o=ord
 for a in sys.argv[1:]:
  c,r,f=a[0],abs(o(a[3])-o(a[1])),abs(o(a[4])-o(a[2]))
- if c=='K'and {r,f}.issubset({0,1})or c=='B'and r==f or c=='N'and{r,f}=={1,2}or c=='R'and 0in{r,f}or c=='Q'and(r+f==1 or r==f or 0in{r,f}):print(a)
+ if c in'QK'and r<2and f<2or c in'BQ'and r==f or c=='N'and{r,f}=={1,2}or c in'RQ'and 0in{r,f}:print(a)
